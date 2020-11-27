ff0627870f5a6b65a87305f27e2640349fd980b1
wanthook <taufiq@indahjaya.co.id>
2020-11-27 15:00:52 +0700

    Ketinggalan

---
+++ app/PivotRequestAlasan.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Relations\Pivot;
+use Auth;
+use App\RequestAlasan;
+
+class PivotRequestAlasan extends Pivot
+{
+    /**
+     * Convert the amount from pence to pounds.
+     *
+     * @param $amount
+     * @return float|int
+     */
+    public function getRequestAlasanIdAttribute($id)
+    {
+        $ret = null;
+        if($id)
+        {
+            $ret = RequestAlasan::find($id);
+        }
+
+        return $ret;
+    }
+}
