3698357cd50ea4424fe879b0602cdeb02c0c8a95
wanthook <taufiq@indahjaya.co.id>
2021-07-16 19:26:54 +0700

    Hapus google font

--- resources/views/adminlte3/login.blade.php
+++ resources/views/adminlte3/login.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="icon" type="image/png" href="{{asset('favicon.ico')}}"/>
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
+    <!-- <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet"> -->
 
     <link rel="stylesheet" href="{{asset('plugins/loginv06/fonts/icomoon/style.css')}}">
 
